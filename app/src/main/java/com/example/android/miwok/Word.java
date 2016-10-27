b4dad1cb60ca20f986ba35459a94b3d7e2a1ea2c
Isabelle Giuong <isabelle.giuong@gmail.com>
2016-10-26 22:10:31 -0500

    Hook audio for NumbersActivity, PhrasesActivity, and Word java files.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -12,16 +12,19 @@ public class Word {
     private String mMiwokTranslation;
     private int mImageResourceID;
     private static final int NO_IMAGE_AVAILABLE = -1;
+    private int mAudioResourceID;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceID) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceID = NO_IMAGE_AVAILABLE;
+        mAudioResourceID = audioResourceID;
     }
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID, int audioResourceID) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceID = imageResourceID;
+        mAudioResourceID = audioResourceID;
     }
     public String getDefaultTranslation() {
         return mDefaultTranslation;
@@ -32,7 +35,11 @@ public class Word {
     public int getImageResourceID() {
         return mImageResourceID;
     }
+    public int getAudioResourceID(){
+        return mAudioResourceID;
+    }
     public boolean imageAvailable(){
         return mImageResourceID !=NO_IMAGE_AVAILABLE;
     }
+
 }
